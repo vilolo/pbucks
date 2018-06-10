f2decb564f948951899f4e6da6fd389712d616dc
feng <764523295@qq.com>
2018-06-10 14:55:01 +0800

    udpate

--- environments/prod/backend/config/params-local.php
+++ environments/prod/backend/config/params-local.php
@@ -1,3 +1,8 @@
 <?php
 return [
+    'app_key' => '85407212-bb1c-4de1-87be-2c34a4df36c5',
+    'secret_key' => "60AE7CE93B827ED29B25E97CAB9E3EFC",
+    'contract_type' => "this_week",
+    'symbol' => "ltc_usd",
+    'coin_type' => 'ltc'
 ];
